aa508392ba89107721d503cf0c6dc01a18e1b5d0
coutbiaafnts <bia.fontes653@gmail.com>
2024-09-06 19:24:55 -0300

    feat: insere dados na tabela de produto

---
+++ DB_ADigital/insert/insert_into_produto.sql
@@ -0,0 +1,42 @@
+INSERT INTO
+  produto (
+    nome,
+    categoria,
+    quantidade_estq,
+    quantidade_min,
+    quantidade_perdida,
+    preco_venda,
+    preco_compra,
+    data_cad_prod
+  )
+VALUES
+  (
+    'Produto A',
+    'Eletrônicos',
+    50,
+    10,
+    2,
+    150.00,
+    100.00,
+    '2024-06-09 21:37'
+  ),
+  (
+    'Produto B',
+    'Vestuário',
+    30,
+    5,
+    1,
+    80.00,
+    50.00,
+    '2024-06-09 21:37'
+  ),
+  (
+    'Produto C',
+    'Alimentos',
+    100,
+    20,
+    5,
+    20.00,
+    10.00,
+    '2024-06-09 21:37'
+  );
